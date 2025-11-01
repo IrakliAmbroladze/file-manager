916b7325ec74c31dd0d654dd695fe8737d40455b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-01 16:35:11 +0400

    feat: add completer in create interface

--- src/utils/startCLI.js
+++ src/utils/startCLI.js
@@ -1,11 +1,21 @@
 import { homedir } from "node:os";
 import { createInterface } from "node:readline/promises";
 
+const COMPLITIONS =
+  ".exit up cd ls cat add mkdir rn cp mv rm os hash compress decompress".split(
+    " ",
+  );
+function completer(line) {
+  const hits = COMPLITIONS.filter((c) => c.startsWith(line));
+  return [hits.length ? hits : COMPLITIONS, line];
+}
+
 export const startCLI = async () => {
   console.log("You are currently in", homedir(), "\n");
   const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
+    completer,
   });
   rl.on("line", (line) => {
     if (line.trim() === ".exit") {
